5caff74adf0db70aedf20165847189abc73ac315
Jiří Sejkora <sejseloid@gmail.com>
2018-05-18 10:04:37 +0200

    PPL: Improved code, error messages

--- handlers_ppl.go
+++ handlers_ppl.go
@@ -16,11 +16,11 @@ type PplTrackingData struct {
 	Location string
 }
 
-var (
-	PACKAGE_1 = PplTrackingData{"55604070459", "Horní Sytová", "512 41", "TODO Odesílatel", "0,1 kg", "Balíček je v hajzlu. První patro, třetí dveře vpravo."} // TODO: Pozice
-	PACKAGE_2 = PplTrackingData{"16145962924", "Horní Sytová", "512 41", "TODO Odesílatel", "0,15 kg", "Balíček je určen na podpal."} // TODO: Pozice
-	PACKAGE_3 = PplTrackingData{"44467405130", "Horní Sytová", "512 41", "TODO Odesílatel", "0,19 kg", "Balíček je neumístěn."} // TODO: Pozice
-)
+var packages = []PplTrackingData{
+	PplTrackingData{"55604070459", "Horní Sytová", "512 41", "TODO Odesílatel", "0,1 kg", "Balíček je v hajzlu. První patro, třetí dveře vpravo."},
+	PplTrackingData{"16145962924", "Horní Sytová", "512 41", "TODO Odesílatel", "0,15 kg", "Balíček je určen na podpal."},
+	PplTrackingData{"44467405130", "Horní Sytová", "512 41", "TODO Odesílatel", "0,19 kg", "Balíček je neumístěn."},
+}
 
 type pplTrackingPageData struct {
 	GeneralData
@@ -46,41 +46,31 @@ func pplTrackingHandler(w http.ResponseWriter, r *http.Request) {
 	code := r.URL.Query().Get("zasilka_id")
 	log.Debugf("Zasilka id: %s", code);
 
-	if code == PACKAGE_1.Code {
-		if (team.PPL.PackagesTracked == 0) {
-			team.PPL.PackagesTracked = 1
-			log.Infof("[PPL - %s] Tracked package 1", team.Login)
-		}
-		if (team.PPL.PackagesTracked >= 1) {
-			data.PplTrackingData = PACKAGE_1
-		}
-	} else if code == PACKAGE_2.Code {
-		if (team.PPL.PackagesTracked == 1) {
-			team.PPL.PackagesTracked = 2
-			log.Infof("[PPL - %s] Tracked package 2", team.Login)
-		}
-		if (team.PPL.PackagesTracked >= 2) {
-			data.PplTrackingData = PACKAGE_2
-		} else {
-			log.Infof("[PPL - %s] Tried to track package 2 without discovering package 1", team.Login)
-		}
-	} else if code == PACKAGE_3.Code {
-		if (team.PPL.PackagesTracked == 2) {
-			team.PPL.PackagesTracked = 3
-			log.Infof("[PPL - %s] Tracked package 3", team.Login)
-
-			team.PPL.Completed = true
-			team.PPL.CompletedTime = time.Now()
-		}
-		if (team.PPL.PackagesTracked >= 3) {
-			data.PplTrackingData = PACKAGE_3
-		} else {
-			log.Infof("[PPL - %s] Tried to track package 3 without discovering package 2", team.Login)
+	for i, pack := range packages {
+		if code == pack.Code {
+			if team.PPL.PackagesTracked == i {
+				team.PPL.PackagesTracked = i + 1
+				log.Infof("[PPL - %s] Tracked package %i", team.Login, i + 1)
+			}
+			if (team.PPL.PackagesTracked >= i + 1) {
+				data.PplTrackingData = pack
+			} else {
+				data.Message = "Zásilka nebyla nalezena kvůli chybě v časoprostorovém kontinuu."
+				log.Infof("[PPL - %s] Tried to track package %i without discovering package %i", team.Login, i + 1, team.PPL.PackagesTracked + 1)
+			}
 		}
 	}
-	if (data.PplTrackingData.Code != "") {
+	if team.PPL.PackagesTracked == 3 && !team.PPL.Completed {
+			team.PPL.Completed = true
+			team.PPL.CompletedTime = time.Now()
+	}
+
+	if data.PplTrackingData.Code != "" {
 		defer func() { executeTemplate(w, "pplTracking", data) }()
 	} else {
-	defer func() { executeTemplate(w, "pplPackageNotFound", data) }()
+		if data.Message == "" {
+			data.Message = "Zásilka nebyla nalezena"
+		}
+		defer func() { executeTemplate(w, "pplPackageNotFound", data) }()
 	}
 }
